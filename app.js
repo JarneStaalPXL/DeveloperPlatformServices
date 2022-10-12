58dc53cb92604b742c2690c059adf365e64611bb
DeveloperJarne <12001144@student.pxl.be>
2022-10-12 07:14:13 +0200

    fix

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const routes = require('./routes');
 const app = express();
 
 app.use(express.static('public'));
-app.use('/', routes);
+app.use('/convert', routes);
 
 module.exports = app    
--- routes.js
+++ routes.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-
+//create a post request with body
+router.get('/DocToPdf', function(req, res) {
+    let data = req.body;
+    console.log(data);
+});
 
 module.exports = router
